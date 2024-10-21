2430bf9a4fa99a40757999566e4f47631de24b3a
arvee0220 <rowellverdejo@gmail.com>
2024-10-22 03:49:23 +0800

    Push activity solution for a14

--- activities/src/bin/a14.rs
+++ activities/src/bin/a14.rs
@@ -11,4 +11,52 @@
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+// * Use a struct for a persons age, name, and favorite color
+struct Person {
+    age: u8,
+    name: String,           // * The color and name should be stored as a String
+    favorite_color: String, // * The color and name should be stored as a String
+}
+
+/* impl Person {
+    fn print_name(&self) {
+        println!("{:}", self.name)
+    }
+
+    fn print_color(&self) {
+        println!("{:}", self.favorite_color)
+    }
+} */
+
+fn print(data: &str) {
+    println!("{data:}")
+}
+
+fn main() {
+    let people: Vec<Person> = vec![
+        Person {
+            age: 21,
+            name: String::from("Sky"),
+            favorite_color: String::from("Blue"),
+        },
+        Person {
+            age: 10,
+            name: String::from("Heart"),
+            favorite_color: String::from("Pink"),
+        },
+        Person {
+            age: 25,
+            name: String::from("Chen"),
+            favorite_color: String::from("Violet"),
+        },
+    ];
+
+    // * Iterate through the vector using a for..in loop
+    for person in &people {
+        // * Use an if expression to determine which person's info should be printed
+        if person.age <= 10 {
+            print(&person.name);
+            print(&person.favorite_color);
+        }
+    }
+}
