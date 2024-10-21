38f805a819c0be47149663ade430134c368d1842
arvee0220 <rowellverdejo@gmail.com>
2024-10-22 03:07:02 +0800

    Push activity solution for a13

--- activities/src/bin/a13.rs
+++ activities/src/bin/a13.rs
@@ -10,4 +10,22 @@
 // * Determine whether to print the number or print "thirty" inside the loop
 // * Use the .len() function to print the number of elements in a vector
 
-fn main() {}
+fn main() {
+    // * Use a vector to store 4 numbers
+    let my_vec: Vec<i64> = vec![10, 20, 30, 40];
+
+    // * Iterate through the vector using a for..in loop
+    for i in &my_vec {
+        // * Determine whether to print the number or print "thirty" inside the loop
+        if *i == 30 {
+            println!("thirty")
+        } else {
+            println!("{i:}")
+        }
+    }
+
+    let vec_len = my_vec.len();
+
+    // * Use the .len() function to print the number of elements in a vector
+    println!("my_vec length is {vec_len:}")
+}
