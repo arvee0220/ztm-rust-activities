6d5ef4dd569362c660bfaff34bb0d1be2b728985
arvee0220 <rowellverdejo@gmail.com>
2024-10-24 06:02:17 +0800

    Commit activity solution for a17

--- activities/src/bin/a17.rs
+++ activities/src/bin/a17.rs
@@ -12,4 +12,12 @@
 //   to uppercase and lowercase
 //   * Try searching for: to_uppercase, to_lowercase
 
-fn main() {}
+fn main() {
+    let myself = "ROWELL";
+
+    let lower = &myself.to_lowercase();
+
+    let upper = &lower.to_uppercase();
+
+    println!("lower case: {lower:}, Upper case: {upper:}");
+}
