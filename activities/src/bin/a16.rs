7de121bc4dfef1a6ee63ec3f665ca2a5f53c12a2
arvee0220 <rowellverdejo@gmail.com>
2024-10-24 05:12:17 +0800

    Push activity solution for a16

--- activities/src/bin/a16.rs
+++ activities/src/bin/a16.rs
@@ -7,8 +7,25 @@
 // Notes:
 // * Use a struct containing the student's name and locker assignment
 // * The locker assignment should use an Option<i32>
-
 // * Use a struct containing the student's name and locker assignment
 // * The locker assignment should use an Option<i32>
 
-fn main() {}
+// * Use a struct containing the student's name and locker assignment
+struct Student {
+    name: String,
+    // * The locker assignment should use an Option<i32>
+    locker: Option<i32>,
+}
+fn main() {
+    let rv: Student = Student {
+        name: String::from("Rowell"),
+        locker: Some(5),
+    };
+
+    println!("Student name: {:}", rv.name);
+
+    match rv.locker {
+        Some(locker) => println!("Locker assignment number: {locker:}"),
+        None => println!("No locker assigned"),
+    }
+}
